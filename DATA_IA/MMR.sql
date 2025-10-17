6167c6e14ef759a3e64ef57256faa3e04650adea
JulienFromont <96061482+JulienFromont@users.noreply.github.com>
2025-10-17 19:52:16 +0200

    Update MMR.sql

--- DATA_IA/MMR.sql
+++ DATA_IA/MMR.sql
@@ -27,3 +27,28 @@ WHERE EXISTS (
     FROM performances perf
     WHERE perf.player_id = players.player_id
 );
+
+
+
+-- Pour voir qui as le meilleurs MMR
+
+
+SELECT 
+    p.player_id,
+    p.player_name,
+    SUM(
+        CASE
+            WHEN perf.team_color = g.winner THEN 30
+            ELSE -25
+        END
+        + (perf.goals * 4)
+        + (perf.assist * 2)
+        + (perf.player_saves * 2)
+        - (perf.own_goals * 5)
+        + (ABS(g.score_red - g.score_blue) / 3.0)
+    ) AS MMR
+FROM players p
+JOIN performances perf ON p.player_id = perf.player_id
+JOIN games g ON perf.game_id = g.game_id
+GROUP BY p.player_id, p.player_name
+ORDER BY MMR DESC;
